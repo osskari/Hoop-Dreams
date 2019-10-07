2b9943652843d92e94d595cc06e0ca3352bb028e
osskari <oskarjorgen@gmail.com>
2019-10-07 16:03:43 +0000

    db ready

--- data/db.js
+++ data/db.js
@@ -4,23 +4,22 @@ const playerSchema = require('../schema/types/player').mongoDb;
 const pickupGameSchema = require('../schema/types/pickupGame').mongoDb;
 const playersInGamesSchema = require('../schema/types/playersInGames').mongoDb;
 
-const connectionString = 'mongodb+srv://admin:Admin12345@hoops-aj2kw.mongodb.net/test?retryWrites=true&w=majority';
-const hebbo = 'mongodb+srv://hebbo:Hebbo1337@hoops-aj2kw.mongodb.net/Hoops?retryWrites=true&w=majority';
-const connection = mongoose.createConnection(hebbo, { useNewUrlParser: true, useUnifiedTopology: true });
+const connectionString = 'mongodb+srv://admin:Admin12345@hoops-aj2kw.mongodb.net/Hoops?retryWrites=true&w=majority';
+const connection = mongoose.createConnection(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 module.exports = {
-    // basketballFields: (id) => {
-    //     let uri = 'https://basketball-fields.herokuapp.com/api/basketball-fields';
-    //     if(id != undefined){ uri += `/${id}` }
-    //     return requestPromise({
-    //     uri: uri,
-    //     headers: {
-    //         'User-Agent': 'Request-Promise'
-    //     },
-    //     json: true
-    // }
-    // )},
+    basketballFields: (id) => {
+        let uri = 'https://basketball-fields.herokuapp.com/api/basketball-fields';
+        if(id != undefined){ uri += `/${id}` }
+        return requestPromise({
+        uri: uri,
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+        json: true
+    }
+    )},
     PickupGame: connection.model('PickupGame', pickupGameSchema, 'PickupGames'),
     Player: connection.model('Player', playerSchema, 'Players'),
     PlayersInGame: connection.model('PlayersInGame', playersInGamesSchema, 'PlayersInGames')
--- test.js
+++
@@ -1,8 +0,0 @@
-const {Player} = require('./data/db');
-
-
-
-Player.find({}, (err, players) => {
-    if (err) { throw new Error(err);}
-    console.log("Hebbo!!: ", players);
-})
