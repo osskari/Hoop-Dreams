d4d873b6d48d2d005267cf91bb4d724e0beccebf
osskari <oskarjorgen@gmail.com>
2019-10-08 11:43:35 +0000

    basketballFields split from db.js

--- data/db.js
+++ data/db.js
@@ -1,4 +1,3 @@
-const requestPromise = require('request-promise');
 const mongoose = require('mongoose');
 const playerSchema = require('../schema/types/player').mongoDb;
 const pickupGameSchema = require('../schema/types/pickupGame').mongoDb;
@@ -9,17 +8,6 @@ const connection = mongoose.createConnection(connectionString, { useNewUrlParser
 
 
 module.exports = {
-    basketballFields: (id) => {
-        let uri = 'https://basketball-fields.herokuapp.com/api/basketball-fields';
-        if(id != undefined){ uri += `/${id}` }
-        return requestPromise({
-        uri: uri,
-        headers: {
-            'User-Agent': 'Request-Promise'
-        },
-        json: true
-    }
-    )},
     PickupGame: connection.model('PickupGame', pickupGameSchema, 'PickupGames'),
     Player: connection.model('Player', playerSchema, 'Players'),
     PlayersInGame: connection.model('PlayersInGame', playersInGamesSchema, 'PlayersInGames')
--- services/basketballFieldService.js
+++ services/basketballFieldService.js
@@ -1,4 +1,4 @@
-const { basketballFields } = require('../data/db');
+const requestPromise = require('request-promise');
 const errors = require('../errors');
 const Moment = require('moment');
 
@@ -6,7 +6,13 @@ const BasketballFieldService = () => {
     const getAllBasketballFields = async () => {
         let arr = [];
         try {
-            await basketballFields().then( data => {
+            await requestPromise({
+                uri: 'https://basketball-fields.herokuapp.com/api/basketball-fields',
+                headers: {
+                    'User-Agent': 'Request-Promise'
+                },
+                json: true
+            }).then(data => {
                 data.forEach(i => {
                     arr.push({
                         id: i.id,
@@ -22,12 +28,17 @@ const BasketballFieldService = () => {
         }
         return arr;
     };
-
     const getBasketballFieldById = async (id) => {
         if(!id){ throw new errors.UserInputError("No id given"); }
         let ret;
         try {
-            await basketballFields(id).then( data => {
+            await requestPromise({
+                uri: `https://basketball-fields.herokuapp.com/api/basketball-fields/${id}`,
+                headers: {
+                    'User-Agent': 'Request-Promise'
+                },
+                json: true
+            }).then( data => {
                 ret = {
                     id: data.id,
                     name: data.name,
@@ -36,7 +47,7 @@ const BasketballFieldService = () => {
                     status: data.status,
                 };
             });
-        } catch(err){
+        } catch(err) {
             if (err == 'StatusCodeError: 404 - "Basketball field was not with this id."'){
                 throw new errors.UserInputError(err);
             }
@@ -52,15 +63,23 @@ const BasketballFieldService = () => {
 
 module.exports = BasketballFieldService();
 
-// BasketballFieldService().getAllBasketballFields().then(data => {
-//     console.log(data);
-// });
+// Examples
 
-// BasketballFieldService().getBasketballFieldById()
-// .then(data => {
+// BasketballFieldService().getAllBasketballFields().then(data => {
+// //     Do stuff
 //     console.log(data);
 // })
 // .catch(err => {
-//     console.log(err);
+// //     Handle errors
+//     console.log(err)
 // });
 
+// BasketballFieldService().getBasketballFieldById('647ffc67-265c-40a4-84c9-ccdcd2fdeac7')
+// .then(data => {
+// //     Do stuff
+//     console.log(data);
+// })
+// .catch(err => {
+// //     Handle errors
+//     console.log(err);
+// });
